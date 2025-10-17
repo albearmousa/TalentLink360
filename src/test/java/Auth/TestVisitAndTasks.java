78c80f025f20083303476c8d96358214de87704f
albearmousa <albear.mousa@galaxysoft.ws>
2025-10-17 17:37:30 +0300

    Visit tested and navigates to home screen

--- src/test/java/Auth/TestVisitAndTasks.java
+++ src/test/java/Auth/TestVisitAndTasks.java
@@ -1,13 +1,14 @@
 package Auth;
 
 import base.TestBase;
+import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.PointerInput;
 import org.openqa.selenium.interactions.Sequence;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.Arrays;
 import java.util.Collections;
 
 public class TestVisitAndTasks extends TestBase {
@@ -83,6 +84,67 @@ public class TestVisitAndTasks extends TestBase {
         driver.findElement(By.id("Submit")).click();
         driver.findElement(By.id("Ok")).click();
 
-    }
+        // Visit details
+        driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeOther[`name CONTAINS 'Pending'`][1]")).click();
 
+        // Three points
+        driver.findElement(By.xpath("//XCUIElementTypeWindow/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeImage[2]")).click();
+
+        // Add comment
+        driver.findElement(By.id("Add Comment")).click();
+
+        // Write a comment
+        driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeWindow/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeTextField")).sendKeys("Add a new comment");
+
+        // Submit comment
+        driver.findElement(By.id("Add Comment")).click();
+
+        // Check-in
+        driver.findElement(By.id("Check In")).click();
+
+        WebElement checkoutMessage = driver.findElement(By.name("Data saved Successfully"));
+
+        String messageText = checkoutMessage.getAttribute("value");
+        System.out.println("✓ Successfully verified checkout message: " + messageText);
+
+        driver.findElement(By.name("Ok")).click();
+        System.out.println("✓ Successfully tapped 'Ok' button check-in");
+
+        // Check-out
+        WebElement button = driver.findElement(By.id("Check Out"));
+        if (button.getText().equals("Check Out")) {
+            button.click();
+        }
+        driver.findElement(By.name("Ok")).click();
+        System.out.println("✓ Successfully tapped 'Ok' button check-out");
+
+        // Start
+        driver.findElement(By.id("Start")).click();
+        WebElement startMessage = driver.findElement(By.name("Data saved Successfully"));
+
+        String startMessageText = startMessage.getAttribute("value");
+        System.out.println("✓ Successfully verified checkout message: " + startMessageText);
+
+        driver.findElement(By.name("Ok")).click();
+        System.out.println("✓ Successfully tapped 'Ok' button Start");
+
+        // End
+        WebElement endButton = driver.findElement(By.id("End"));
+        if (endButton.getText().equals("End")) {
+            endButton.click();
+        }
+        driver.findElement(By.name("Ok")).click();
+        System.out.println("✓ Successfully tapped 'Ok' button End");
+
+        // Back to Home
+
+        driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeWindow/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeImage[1]")).click();
+
+        driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeWindow/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeImage")).click();
+
+        driver.findElement(By.id("Home\\nTab 1 of 5")).click();
+
+
+
+    }
 }
